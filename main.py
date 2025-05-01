9d7ed3e3928bde24d850ee309371b1399783da37
21Tomi-polsl <tm306071@student.polsl.pl>
2025-05-01 12:04:31 +0200

    added basic "predict" button funcitons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import yfinance as yf
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PySide6.QtCore import QFileInfo
@@ -11,14 +12,34 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.fileName.setVisible(False)
         self.ui.loadCSV.clicked.connect(self.open_dialog)
+        self.ui.predict.clicked.connect(self.show_data)
+
 
     def open_dialog(self):
         fname = QFileDialog.getOpenFileName(self, "Open File", "/", "CSV Files (*.csv)")
         self.ui.fileName.setVisible(True)
         fi = QFileInfo(fname[0])
         name = fi.fileName()
-        print(fname)
         self.ui.fileName.setText(name)
+
+
+    def show_data(self):
+        tick = "AAPL"
+        p1 = financeAPI(tick)
+        p1.showInfo()
+
+
+class financeAPI:
+    def __init__(self, ticker):
+        self.ticker = yf.Ticker(ticker)
+
+    def showInfo(self):
+        his_data = self.ticker.history(period="1y")
+        print(type(his_data))
+        print(his_data)
+
+
+
 
 def main():
     app = QApplication(sys.argv)
@@ -28,4 +49,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
